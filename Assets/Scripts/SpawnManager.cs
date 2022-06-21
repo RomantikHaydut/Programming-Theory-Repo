cb5145744e6a62b64057af6a9a690f742af4f8dd
Huseyin <akcali-1997@hotmail.com>
2022-06-21 12:35:48 +0300

    commit
    boss prefab ready

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
