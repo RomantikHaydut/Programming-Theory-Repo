ece767e0e2c6c1502acdb1ede4a076b63021b909
Huseyin <akcali-1997@hotmail.com>
2022-06-23 15:43:37 +0300

    commit
    Spawn Enemy method ok.
    Prevented spawnin near the player in EnemyController.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+
+    private float speed;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        speed = 2;
+        ProtectSpawnNearPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+        FollowPlayer();
     }
+
+    void FollowPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        transform.position += direction*Time.deltaTime*speed;
+    }
+
+    void ProtectSpawnNearPlayer()
+    {
+        //Here in spawning if enemy spawns near the player we get away it from player..
+        float spawnDistance= Vector3.Distance(player.transform.position, transform.position);
+        if (spawnDistance<=22f)
+        {
+            Vector3 direction = (transform.position - player.transform.position).normalized;
+            transform.position += new Vector3(direction.x,0,direction.z) * 40f;
+
+        }
+    }
+
+
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
+
+    private Vector3 randomSpawnPos;
+
+    private float xRange = 100;
+
+    private float zRange = 100;
+
+    private int enemyCount;
     void Start()
     {
-        
+        enemyCount = 0;
+        InvokeRepeating("SpawnEnemy", 1f, 0.1f);
     }
 
-    
+
     void Update()
     {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemyCount<=50)
+        {
+            int index = Random.Range(0, enemyPrefabs.Length);
+            Instantiate(enemyPrefabs[index], SpawnPos(), enemyPrefabs[index].transform.rotation);
+        }
         
     }
 
+    Vector3 SpawnPos()
+    {
+        randomSpawnPos = new Vector3(Random.Range(-xRange, xRange), 1, Random.Range(-zRange, zRange));
 
+        return randomSpawnPos;
+    }
 
 
 }
