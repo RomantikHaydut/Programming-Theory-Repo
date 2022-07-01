d11e3864b1ee0c568a3b5eb96fa981e9e0327089
Huseyin <akcali-1997@hotmail.com>
2022-07-01 05:03:46 +0300

    commit.
    Little reworks on options.

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -7,50 +7,84 @@ using TMPro;
 public class CanvasManager : MonoBehaviour
 {
     public GameObject panel;
-    public TextMeshProUGUI[] options;
+    public GameObject[] options;
+    public GameManager gameManager;
 
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ShowOptions();
+        }
+    }
     public void ShowOptions()
     {
         Time.timeScale = 0;
         panel.SetActive(true);
     }
 
+    private void CloseOptions()
+    {
+        Time.timeScale = 1;
+        panel.SetActive(false);
+    }
+
     public void IncreaseBombEffectRadius()
     {
+        gameManager.SelectCapsule();
         Bomb.effectRadius *= 2;
+        CloseOptions();
     }
 
     public void SecondHammer()
     {
+        gameManager.SelectCube();
         FindObjectOfType<CubeController>().secondHammerPowerup = true;
         FindObjectOfType<CubeController>().powerup = true;
-
+        FindObjectOfType<CameraController>().GetPlayer();
+        options[1].gameObject.SetActive(true);
+        options[2].gameObject.SetActive(false);
+        CloseOptions();
     }
 
     public void FasterHammer()
     {
         Hammer.speed = 200;
+        options[1].gameObject.SetActive(false);
+        CloseOptions();
+
     }
 
     public void PlayerCooldown()
     {
         GameManager.secondChangePlayerTime = 3f;
+        options[0].gameObject.SetActive(false);
+        CloseOptions();
+
     }
 
     public void AbilityCooldown()
     {
-        CubeController.hammerDisappearTime = -1f;
         SphereController.cooldown = 1.5f;
         CapsuleController.cooldown = 1.5f;
+        CloseOptions();
+        options[6].gameObject.SetActive(false);
     }
 
     public void ThrowMoreBumerang()
     {
+        gameManager.SelectSphere();
         if (SphereController.bumerangCount < 128)
         {
             SphereController.bumerangCount *= 2;
         }
+        CloseOptions();
+
     }
 
     public void PickAllExperience()
@@ -62,6 +96,8 @@ public class CanvasManager : MonoBehaviour
             StartCoroutine(exp.GetComponent<Experience>().FollowingPlayer());
 
         }
+        CloseOptions();
+
         return;
     }
 }
