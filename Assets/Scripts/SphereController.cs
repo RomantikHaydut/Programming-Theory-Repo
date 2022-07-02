aa616f1e8617e73c0218d9e91c370f038bf88432
Huseyin <akcali-1997@hotmail.com>
2022-07-02 18:13:02 +0300

    commit

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class SphereController : Player
 {
-    private float jumpForce;
-    Rigidbody rb;
     public GameObject bumerangPrefab;
     public static int bumerangCount=4;
     private bool canPowerup;
@@ -16,30 +14,15 @@ public class SphereController : Player
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         moveSpeed = 1.5f;
-        jumpForce = 11.5f;
-        shapaName = "Sphere";
         powerup = true;
         canPowerup = true;
         cooldown = 4f;
         
     }
-    /*public override void FireProjectile(GameObject projectile, bool takeChild)
-    {
-        for (int i = 0; i <= bumerangCount; i++)
-        {
-            projectileSpawnPos = transform.position + transform.up / 4 + new Vector3(Mathf.Sin(i * 90 * Mathf.Deg2Rad), 0, Mathf.Cos(i * 90 * Mathf.Deg2Rad)) * 2;
-            base.FireProjectile(projectile, takeChild);
-            powerup = true;
-            canPowerup = true;
-        }
-
-    }*/
     void FixedUpdate()
     {
         Move(transform);
-        Jump(rb, jumpForce);
 
     }
 
